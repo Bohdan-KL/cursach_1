0603929c0524791ba266c397de26440c454a9c35
Bohdan-KL <bohdanklots@gmail.com>
2020-11-14 18:44:42 +0200

    added tips

--- 1_kivy.py
+++ 1_kivy.py
@@ -9,6 +9,7 @@ from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.screenmanager import RiseInTransition
 from kivy.uix.screenmanager import FallOutTransition
+from kivy.uix.label import Label
 
 Config.set("graphics", "resiziable", '0')
 Config.set("graphics", "width", '640')
@@ -21,48 +22,89 @@ class MenuScreen(Screen):
     pass
 class ChooseScreen(Screen):
     pass
+class ItemsScreen_x(Screen):
+    pass
+class ItemsScreen_y(Screen):
+    pass
+class ItemsScreen_z(Screen):
+    pass
 
 sm = ScreenManager()
 sm.transition=RiseInTransition()
+
 MenuScreen=Screen(name='menu')
-sm.add_widget(MenuScreen)
 ChooseScreen=Screen(name='choose')
+ItemsScreen_x=Screen(name='items_x')
+ItemsScreen_y=Screen(name='items_y')
+ItemsScreen_z=Screen(name='items_z')
+sm.add_widget(MenuScreen)
 sm.add_widget(ChooseScreen)
+sm.add_widget(ItemsScreen_x)
+sm.add_widget(ItemsScreen_y)
+sm.add_widget(ItemsScreen_z)
+
 
 class cursachApp(App):
     def build(self):
-
+############################################################################################################
         def hist_press(self):
             global lim
             global lim1
             if not lim and not lim1:
+                if situation.text == 'Оберіть csv-файл':
+                    situation.text = 'Графік обрано\nОберіть файл'
+                elif situation.text == "Файл обрано\nОберіть вид графіка":
+                    situation.text = 'Файл і графік обрано\nОберіть значення'
                 full_interface.add_widget(hist)
                 lim = True
             elif not lim and lim1:
+                if situation.text == 'Оберіть csv-файл':
+                    situation.text = 'Графік обрано\nОберіть файл'
+                elif situation.text == "Файл обрано\nОберіть вид графіка":
+                    situation.text = 'Файл і графік обрано\nОберіть значення'
                 full_interface.remove_widget(Point_x_y)
                 full_interface.add_widget(hist)
                 lim = True
                 lim1 = False
             elif lim and lim1:
+                if situation.text == 'Оберіть csv-файл':
+                    situation.text = 'Графік обрано\nОберіть файл'
+                elif situation.text == "Файл обрано\nОберіть вид графіка":
+                    situation.text = 'Файл і графік обрано\nОберіть значення'
                 full_interface.remove_widget(Point_x_y)
                 lim1 = False
-
+########################################################################################################
         def point_press(self):
             global lim1
             global lim
             if not lim1 and not lim:
+                if situation.text == 'Оберіть csv-файл':
+                    situation.text = 'Графік обрано\nОберіть файл'
+                elif situation.text == "Файл обрано\nОберіть вид графіка":
+                    situation.text = 'Файл і графік обрано\nОберіть значення'
                 full_interface.add_widget(Point_x_y)
                 lim1 = True
             elif not lim1 and lim:
+                if situation.text == 'Оберіть csv-файл':
+                    situation.text = 'Графік обрано\nОберіть файл'
+                elif situation.text == "Файл обрано\nОберіть вид графіка":
+                    situation.text = 'Файл і графік обрано\nОберіть значення'
                 full_interface.remove_widget(hist)
                 full_interface.add_widget(Point_x_y)
                 lim1 = True
                 lim = False
             elif lim and lim1:
+                if situation.text == 'Оберіть csv-файл':
+                    situation.text = 'Графік обрано\nОберіть файл'
+                elif situation.text == "Файл обрано\nОберіть вид графіка":
+                    situation.text = 'Файл і графік обрано\nОберіть значення'
                 full_interface.remove_widget(hist)
                 lim = False
+#################################################################################################################
         def to_chooser(self):
             sm.current = 'choose'
+
+
 
         Create = AnchorLayout(anchor_x='left', anchor_y="bottom", padding=[25, 25, 25, 25])
         create = Button(text="Створити", size_hint=[0.3, 0.2])
@@ -90,10 +132,15 @@ class cursachApp(App):
         exit = Button(text='Вийти', size_hint=[0.3, 0.2], background_color=[1, 0, 0, 1])
         Exit.add_widget(exit)
 
+        Situation = AnchorLayout(anchor_x='right', anchor_y="top", padding=[25, 25, 25, 25])
+        situation=Label(text='Оберіть csv-файл')
+        Situation.add_widget(situation)
+
         full_interface = FloatLayout(size=(480, 640))
         full_interface.add_widget(Create)
         full_interface.add_widget(Save)
         full_interface.add_widget(Exit)
+        full_interface.add_widget(Situation)
 
         dropdown = DropDown()
 
@@ -131,6 +178,10 @@ class cursachApp(App):
 
         way_to_file=''
         def open_chooser(self, Way_to_file, MouseMotionEvent):
+            if situation.text=="Оберіть csv-файл":
+                situation.text="Файл обрано\nОберіть вид графіка"
+            elif situation.text == 'Графік обрано\nОберіть файл':
+                situation.text = 'Файл і графік обрано\nОберіть значення'
             way_to_file=Way_to_file
             sm.transition = FallOutTransition()
             sm.current = 'menu'
@@ -138,10 +189,12 @@ class cursachApp(App):
 
         Chooser=FileChooserListView(on_submit=open_chooser)
         Chooser.filters=['*.csv']
-
-
         Ch.add_widget(Chooser)
         ChooseScreen.add_widget(Ch)
+        ###########################################################################################
+        #if situation=='Файл і графік обрано\nОберіть значення':
+        #    file=
+        ###########################################################################################
         sm.current = 'menu'
         return sm
 
